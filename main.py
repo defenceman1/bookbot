abacfc27553620085e59f01b7712344b25073800
defenceman1 <defenceman@pm.me>
2025-12-11 22:28:50 +0100

    added a function that counts characters and stores it in a dict

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-from stats import get_num_words
+from stats import get_num_words, num_of_char
 
 
 def main():
-    print(get_num_words("books/frankenstein.txt"))
+    print(num_of_char("books/frankenstein.txt"))
+    """
+    dict = {"first": 1, "second": 2}
+    if "first" in dict:
+        print("exists")
+    else:
+        print("doesn't")
+    """
 
 
 if __name__ == "__main__":
--- stats.py
+++ stats.py
@@ -3,3 +3,15 @@ def get_num_words(filepath):
         file_contents = f.read()
         num_words = len(file_contents.split())
         return f"Found {num_words} total words"
+
+
+def num_of_char(filepath):
+    char_count = {}
+    with open(filepath) as f:
+        file_contents = f.read().lower()
+    for char in file_contents:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    return char_count
